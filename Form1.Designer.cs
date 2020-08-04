6f24cdfa08fe3bd537f13bb680d1bec5b1ef008c
Teddy <mickey23405383@gmail.com>
2020-08-04 22:57:04 +0800

    Function pack

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -33,13 +33,13 @@
             this.loadImageToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
             this.button1 = new System.Windows.Forms.Button();
             this.tempImg = new System.Windows.Forms.PictureBox();
-            this.pictureBox1 = new System.Windows.Forms.PictureBox();
-            this.button2 = new System.Windows.Forms.Button();
-            this.button3 = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
             this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.status = new System.Windows.Forms.Label();
             this.menuStrip1.SuspendLayout();
             ((System.ComponentModel.ISupportInitialize)(this.tempImg)).BeginInit();
-            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
             this.SuspendLayout();
             // 
             // menuStrip1
@@ -50,10 +50,9 @@
             this.menuStrip1.Location = new System.Drawing.Point(0, 0);
             this.menuStrip1.Name = "menuStrip1";
             this.menuStrip1.Padding = new System.Windows.Forms.Padding(8, 3, 0, 3);
-            this.menuStrip1.Size = new System.Drawing.Size(1842, 29);
+            this.menuStrip1.Size = new System.Drawing.Size(1842, 30);
             this.menuStrip1.TabIndex = 1;
             this.menuStrip1.Text = "menuStrip1";
-            this.menuStrip1.ItemClicked += new System.Windows.Forms.ToolStripItemClickedEventHandler(this.FileItemClicked);
             // 
             // fileToolStripMenuItem
             // 
@@ -72,9 +71,10 @@
             // 
             // button1
             // 
-            this.button1.Location = new System.Drawing.Point(729, 826);
+            this.button1.Font = new System.Drawing.Font("微軟正黑體", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
+            this.button1.Location = new System.Drawing.Point(1066, 352);
             this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.Size = new System.Drawing.Size(75, 68);
             this.button1.TabIndex = 4;
             this.button1.Text = "DFT";
             this.button1.UseVisualStyleBackColor = true;
@@ -84,57 +84,66 @@
             // 
             this.tempImg.Location = new System.Drawing.Point(12, 32);
             this.tempImg.Name = "tempImg";
-            this.tempImg.Size = new System.Drawing.Size(792, 788);
+            this.tempImg.Size = new System.Drawing.Size(1020, 788);
             this.tempImg.TabIndex = 3;
             this.tempImg.TabStop = false;
             // 
-            // pictureBox1
+            // textBox1
             // 
-            this.pictureBox1.Location = new System.Drawing.Point(810, 32);
-            this.pictureBox1.Name = "pictureBox1";
-            this.pictureBox1.Size = new System.Drawing.Size(792, 788);
-            this.pictureBox1.TabIndex = 5;
-            this.pictureBox1.TabStop = false;
-            this.pictureBox1.Click += new System.EventHandler(this.pictureBox1_Click);
+            this.textBox1.Location = new System.Drawing.Point(1267, 353);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(100, 25);
+            this.textBox1.TabIndex = 6;
+            this.textBox1.Text = "3";
             // 
-            // button2
+            // textBox2
             // 
-            this.button2.Location = new System.Drawing.Point(810, 826);
-            this.button2.Name = "button2";
-            this.button2.Size = new System.Drawing.Size(105, 23);
-            this.button2.TabIndex = 6;
-            this.button2.Text = "InverseDFT";
-            this.button2.UseVisualStyleBackColor = true;
-            this.button2.Click += new System.EventHandler(this.button2_Click);
-            // 
-            // button3
-            // 
-            this.button3.Location = new System.Drawing.Point(943, 826);
-            this.button3.Name = "button3";
-            this.button3.Size = new System.Drawing.Size(105, 23);
-            this.button3.TabIndex = 7;
-            this.button3.Text = "PSNR";
-            this.button3.UseVisualStyleBackColor = true;
-            this.button3.Click += new System.EventHandler(this.button3_Click);
+            this.textBox2.Location = new System.Drawing.Point(1267, 395);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(100, 25);
+            this.textBox2.TabIndex = 7;
             // 
             // label1
             // 
             this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(1054, 830);
+            this.label1.Font = new System.Drawing.Font("微軟正黑體", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
+            this.label1.Location = new System.Drawing.Point(1159, 356);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(41, 15);
+            this.label1.Size = new System.Drawing.Size(51, 22);
             this.label1.TabIndex = 8;
-            this.label1.Text = "label1";
+            this.label1.Text = "kSize";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("微軟正黑體", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
+            this.label2.Location = new System.Drawing.Point(1159, 398);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(82, 22);
+            this.label2.TabIndex = 9;
+            this.label2.Text = "crossBias";
+            // 
+            // status
+            // 
+            this.status.AutoSize = true;
+            this.status.Font = new System.Drawing.Font("微軟正黑體", 13.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
+            this.status.ForeColor = System.Drawing.Color.Red;
+            this.status.Location = new System.Drawing.Point(1057, 462);
+            this.status.Name = "status";
+            this.status.Size = new System.Drawing.Size(82, 31);
+            this.status.TabIndex = 10;
+            this.status.Text = "status";
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 15F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(1842, 878);
+            this.Controls.Add(this.status);
+            this.Controls.Add(this.label2);
             this.Controls.Add(this.label1);
-            this.Controls.Add(this.button3);
-            this.Controls.Add(this.button2);
-            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.textBox1);
             this.Controls.Add(this.button1);
             this.Controls.Add(this.tempImg);
             this.Controls.Add(this.menuStrip1);
@@ -143,7 +152,6 @@
             this.menuStrip1.ResumeLayout(false);
             this.menuStrip1.PerformLayout();
             ((System.ComponentModel.ISupportInitialize)(this.tempImg)).EndInit();
-            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -156,10 +164,11 @@
         private System.Windows.Forms.ToolStripMenuItem loadImageToolStripMenuItem;
         private System.Windows.Forms.Button button1;
         private System.Windows.Forms.PictureBox tempImg;
-        private System.Windows.Forms.PictureBox pictureBox1;
-        private System.Windows.Forms.Button button2;
-        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textBox2;
         private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label status;
     }
 }
 
--- Form1.cs
+++ Form1.cs
@@ -23,17 +23,147 @@ namespace RemovePaperTexture
     {
 
         Mat srcImage = new Mat();
-        Mat FFTImage = new Mat();
-        Mat newImg = new Mat();
-        Image<Gray, byte> final;
+        int srcW, srcH;
+        //Mat FFTImage = new Mat();
+        //Mat newImg = new Mat();
+        //Image<Gray, byte> final;
         public Form1()
         {
             InitializeComponent();
         }
 
-        private void FileItemClicked(object sender, ToolStripItemClickedEventArgs e)
+        private bool DFT_Remove_Texture_Filter(Mat inputImg, ref Mat resultImg, int ksize, int crossBias)
         {
-            
+            if (ksize % 2 == 0) return false;
+            if (crossBias > inputImg.Width || crossBias > inputImg.Height) return false;
+
+            int m = CvInvoke.GetOptimalDFTSize(inputImg.Rows);
+            int n = CvInvoke.GetOptimalDFTSize(inputImg.Cols);
+
+            Mat padded = new Mat();
+            CvInvoke.CopyMakeBorder(inputImg, padded, 0, m - inputImg.Rows, 0, n - inputImg.Cols, BorderType.Constant);
+            padded.ConvertTo(padded, DepthType.Cv32F);
+            Mat zeroMat = Mat.Zeros(padded.Rows, padded.Cols, DepthType.Cv32F, 1);
+            VectorOfMat matVector = new VectorOfMat();
+            matVector.Push(padded);
+            matVector.Push(zeroMat);
+
+            // make a complex mat
+            Mat complexI = new Mat(padded.Size, DepthType.Cv32F, 2);
+            CvInvoke.Merge(matVector, complexI);
+
+            Mat fourier = new Mat(complexI.Size, DepthType.Cv32F, 2);
+            // do dft
+            CvInvoke.Dft(complexI, fourier, DxtType.Forward, complexI.Rows);
+
+
+            /* temp is to show result of dft
+             * 
+             * DEBUG ONLY
+             * 
+            Mat temp = Magnitude(fourier);
+            temp = new Mat(temp, new Rectangle(0, 0, temp.Cols & -2, temp.Rows & -2));
+            SwitchQuadrants(ref temp);
+            CvInvoke.Normalize(temp, temp, 1.0, 0.0, NormType.MinMax, DepthType.Cv32F);
+            CvInvoke.Imshow("Fourier Transform", temp);
+            *
+            *
+            */
+
+            Mat Real = new Mat(fourier.Size, DepthType.Cv32F, 1);
+            Mat Imaginary = new Mat(fourier.Size, DepthType.Cv32F, 1);
+
+            VectorOfMat channels = new VectorOfMat();
+            CvInvoke.Split(fourier, channels);
+            Real = channels.GetOutputArray().GetMat(0);
+            Imaginary = channels.GetOutputArray().GetMat(1);
+
+
+            SwitchQuadrants(ref Real);
+            //CvInvoke.Normalize(Real, Real, 1.0, 0.0, NormType.MinMax, DepthType.Cv32F);
+            SwitchQuadrants(ref Imaginary);
+            //CvInvoke.Normalize(Imaginary, Imaginary, 1.0, 0.0, NormType.MinMax, DepthType.Cv32F);
+
+            // Array data
+            // convert to image instead of using Mat's data pointer
+            Image<Gray, float> img_R = Real.ToImage<Gray, float>();
+            Image<Gray, float> img_I = Real.ToImage<Gray, float>();
+
+            Array tmpR = Real.GetData();
+            // make a Real Image copy
+            Array realCopy = Real.GetData();
+            Image<Gray, float> copy_R = Real.ToImage<Gray, float>();
+
+            for (int i = 0; i < img_R.Width; i++)
+            {
+                for (int j = 0; j < img_R.Height; j++)
+                {
+                    copy_R.Data[j, i, 0] = (float)realCopy.GetValue(j, i);
+                }
+            }
+
+            try
+            {
+                CvInvoke.MedianBlur(copy_R, copy_R, ksize);
+            }
+            catch
+            {
+                return false;
+            }
+
+            Array tmpI = Imaginary.GetData();
+
+            int Center_w = img_R.Width / 2;
+            int Center_h = img_I.Height / 2;
+
+            for (int i = 0; i < img_R.Width; i++)
+            {
+                for (int j = 0; j < img_R.Height; j++)
+                {
+                    if ((i >= Center_w - crossBias && i <= Center_w + crossBias))
+                    {
+                        img_R.Data[j, i, 0] = (float)tmpR.GetValue(j, i);
+                        img_I.Data[j, i, 0] = (float)tmpI.GetValue(j, i);
+                    }
+                    else if ((j >= Center_h - crossBias && j <= Center_h + crossBias))
+                    {
+                        img_R.Data[j, i, 0] = (float)tmpR.GetValue(j, i);
+                        img_I.Data[j, i, 0] = (float)tmpI.GetValue(j, i);
+
+                    }
+                    else
+                    {
+                        img_R.Data[j, i, 0] = copy_R.Data[j, i, 0];
+                        img_I.Data[j, i, 0] = 0;
+                    }
+
+                }
+            }
+
+            // Image back to Mat
+            // make some merge
+            Mat temp1 = new Mat(img_R.Size, DepthType.Cv32F, 1);
+            Mat temp2 = new Mat(img_R.Size, DepthType.Cv32F, 1);
+            temp1 = img_R.Mat;
+            temp2 = img_I.Mat;
+            VectorOfMat matVectorTemp = new VectorOfMat();
+            matVectorTemp.Push(temp1);
+            matVectorTemp.Push(temp2);
+            Mat merge_mat = new Mat(img_R.Size, DepthType.Cv32F, 2);
+            CvInvoke.Merge(matVectorTemp, merge_mat);
+
+            // DFT inverse
+            CvInvoke.Dft(merge_mat, merge_mat, DxtType.Inverse, merge_mat.Rows);
+            Mat magnitudeImage = MagnitudeInverse(merge_mat);
+
+            CvInvoke.Normalize(magnitudeImage, magnitudeImage, 1.0, 0.0, NormType.MinMax, DepthType.Cv32F);
+            //CvInvoke.Imshow("Fourier Transform Inverse", magnitudeImage);
+            CvInvoke.Normalize(magnitudeImage, magnitudeImage, 0, 255, NormType.MinMax, DepthType.Cv8U);
+            //CvInvoke.Imshow("Fourier Normalize", magnitudeImage);
+
+            resultImg = magnitudeImage;
+
+            return true;
         }
 
         private void loadImageToolStripMenuItem_Click(object sender, EventArgs e)
@@ -45,13 +175,101 @@ namespace RemovePaperTexture
 
                 if (ofd.ShowDialog() == DialogResult.OK)
                 {
-                    srcImage = new Mat(ofd.FileName,0);
-                    tempImg.Image = srcImage.ToImage<Gray, byte>().ToBitmap();
-                    
+                    srcImage = new Mat(ofd.FileName, 0);
+                    tempImg.Image = srcImage.ToBitmap();
+                    srcH = srcImage.Height;
+                    srcW = srcImage.Width;
                 }
-                
+
             }
         }
+
+        private Mat Magnitude(Mat fftData)
+        {
+
+            Mat Real = new Mat(fftData.Size, DepthType.Cv32F, 1);
+
+            Mat Imaginary = new Mat(fftData.Size, DepthType.Cv32F, 1);
+            VectorOfMat channels = new VectorOfMat();
+            CvInvoke.Split(fftData, channels); //将多通道mat分离成几个单通道mat
+            Real = channels.GetOutputArray().GetMat(0);
+            Imaginary = channels.GetOutputArray().GetMat(1);
+            CvInvoke.Pow(Real, 2.0, Real);
+            CvInvoke.Pow(Imaginary, 2.0, Imaginary);
+            CvInvoke.Add(Real, Imaginary, Real);
+            CvInvoke.Pow(Real, 0.5, Real);
+            Mat onesMat = Mat.Ones(Real.Rows, Real.Cols, DepthType.Cv32F, 1);
+            CvInvoke.Add(Real, onesMat, Real);
+            CvInvoke.Log(Real, Real); //求自然对数
+            return Real;
+        }
+        private Mat MagnitudeInverse(Mat fftData)
+        {
+
+            Mat Real = new Mat(fftData.Size, DepthType.Cv32F, 1);
+
+            Mat Imaginary = new Mat(fftData.Size, DepthType.Cv32F, 1);
+            VectorOfMat channels = new VectorOfMat();
+            CvInvoke.Split(fftData, channels);
+            Real = channels.GetOutputArray().GetMat(0);
+            Imaginary = channels.GetOutputArray().GetMat(1);
+
+
+            CvInvoke.Pow(Real, 2.0, Real);
+            CvInvoke.Pow(Imaginary, 2.0, Imaginary);
+            CvInvoke.Add(Real, Imaginary, Real);
+            CvInvoke.Pow(Real, 0.5, Real);
+            Console.WriteLine(Real);
+
+            return Real;
+        }
+
+        private void SwitchQuadrants(ref Mat mat)
+        {
+            int cx = mat.Cols / 2;
+            int cy = mat.Rows / 2;
+            Mat q0 = new Mat(mat, new Rectangle(0, 0, cx, cy));
+            Mat q1 = new Mat(mat, new Rectangle(cx, 0, cx, cy));
+            Mat q2 = new Mat(mat, new Rectangle(0, cy, cx, cy));
+            Mat q3 = new Mat(mat, new Rectangle(cx, cy, cx, cy));
+            Mat temp = new Mat(q0.Size, DepthType.Cv32F, 1);
+
+            q0.CopyTo(temp);
+            q3.CopyTo(q0);
+            temp.CopyTo(q3);
+
+            q1.CopyTo(temp);
+            q2.CopyTo(q1);
+            temp.CopyTo(q2);
+        }
+
+        
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            Mat resultImg = new Mat();
+            if(srcImage.Rows == 0)
+            {
+                status.Text = "Failed, no input Image";
+                return;
+            }
+            if (DFT_Remove_Texture_Filter(srcImage, ref resultImg, Convert.ToInt32(textBox1.Text), Convert.ToInt32(textBox2.Text)))
+            {
+                Image<Gray, byte> Img = resultImg.ToImage<Gray, byte>();
+                Img.ROI = new Rectangle(0, 0, srcW, srcH);
+                Image<Gray, byte> cropResult = Img.Copy();
+                CvInvoke.Imshow("Result Image", cropResult);
+                status.Text = "Succeeded";
+            }
+            else
+            {
+                status.Text = "Failed, kSize error(or crossBias error)";
+            }
+            
+        }
+
+        /*
+        
 
         // DFT button
         private void button1_Click(object sender, EventArgs e)
@@ -194,46 +412,6 @@ namespace RemovePaperTexture
             
         }
 
-        private Mat Magnitude(Mat fftData)
-        {
-            
-            Mat Real = new Mat(fftData.Size, DepthType.Cv32F, 1);
-            
-            Mat Imaginary = new Mat(fftData.Size, DepthType.Cv32F, 1);
-            VectorOfMat channels = new VectorOfMat();
-            CvInvoke.Split(fftData, channels); //将多通道mat分离成几个单通道mat
-            Real = channels.GetOutputArray().GetMat(0);
-            Imaginary = channels.GetOutputArray().GetMat(1);
-            CvInvoke.Pow(Real, 2.0, Real);
-            CvInvoke.Pow(Imaginary, 2.0, Imaginary);
-            CvInvoke.Add(Real, Imaginary, Real);
-            CvInvoke.Pow(Real, 0.5, Real);
-            Mat onesMat = Mat.Ones(Real.Rows, Real.Cols, DepthType.Cv32F, 1);
-            CvInvoke.Add(Real, onesMat, Real);
-            CvInvoke.Log(Real, Real); //求自然对数
-            return Real;
-        }
-        private Mat MagnitudeInverse(Mat fftData)
-        {
-           
-            Mat Real = new Mat(fftData.Size, DepthType.Cv32F, 1);
-            
-            Mat Imaginary = new Mat(fftData.Size, DepthType.Cv32F, 1);
-            VectorOfMat channels = new VectorOfMat();
-            CvInvoke.Split(fftData, channels); 
-            Real = channels.GetOutputArray().GetMat(0);
-            Imaginary = channels.GetOutputArray().GetMat(1);
-
-            
-            CvInvoke.Pow(Real, 2.0, Real);
-            CvInvoke.Pow(Imaginary, 2.0, Imaginary);
-            CvInvoke.Add(Real, Imaginary, Real);
-            CvInvoke.Pow(Real, 0.5, Real);
-            Console.WriteLine(Real);
-
-            return Real;
-        }
-
         private void SwitchQuadrants(ref Mat mat)
         {
             int cx = mat.Cols / 2;
@@ -252,9 +430,6 @@ namespace RemovePaperTexture
             q2.CopyTo(q1);
             temp.CopyTo(q2);
         }
-
-
-        
 
         // Inverse button 
         private void button2_Click(object sender, EventArgs e)
@@ -290,9 +465,11 @@ namespace RemovePaperTexture
         // PSNR
         private void button3_Click(object sender, EventArgs e)
         {
-            
+
             label1.Text = Convert.ToString( CvInvoke.PSNR(srcImage, newImg));
         }
+        */
+
     }
 }
 
